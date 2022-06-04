6d49e51fa9513de83116555864c20c0f0834feb9
ZJW <330431976@qq.com>
2022-06-04 17:20:48 +0800

    Upload the init files

---
+++ loop.py
@@ -0,0 +1,17 @@
+counter = 0
+while counter < 10:
+    counter += 1
+    print(counter)
+else:
+    print("I am here")
+
+a = [(1,3,5),["apple","baa","org","lemon"]]
+for x in a:
+    for y in x:
+        if y == 5:
+            break;
+        print(y)
+else:
+    print("I am not here")
+
+    
---
+++ vs_hello.py
@@ -0,0 +1,10 @@
+from _typeshed import IdentityFunction
+
+
+a = input()
+
+if int(a) == 1:
+    print("I am here")
+elif int(a) == 2:
+    print("I am not here")
+  
