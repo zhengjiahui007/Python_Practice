744b9341ebcdfc22e020cc7c01ed820b6257eb85
ZJW <330431976@qq.com>
2022-06-06 22:09:34 +0800

    add if while

---
+++ if_while.py
@@ -0,0 +1,17 @@
+""" 
+ACCOUNT = 'GarryZheng'
+PASSWORD = '12345678'
+
+print('please input account')
+user_account = input()
+
+print('please input password')
+user_password = input()
+
+if (user_account == ACCOUNT) and (user_password == PASSWORD) :
+    print('Login successfully !')
+else :
+    print('Login failed !') 
+"""
+
+#snippet 片段
