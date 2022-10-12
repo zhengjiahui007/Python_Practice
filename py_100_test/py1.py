edce21733b01731b9f263e5e4822001d17e458ef
ZJW <330431976@qq.com>
2022-10-12 22:07:13 +0800

    upload practice 11 12

--- py_100_test/py1.py
+++ py_100_test/py1.py
@@ -1,1 +1,18 @@
-#Practice 11
+#Practice 11 使用1行代码合并字典
+""" first_dict = {1:'A',2:'B',"name":"Garry","location":"Shenzhen"}
+second_dict = {2:'B',"Job":"Software Engineer",3:'C',"Habit":"Badminton"}
+
+dict_result = first_dict | second_dict
+
+print(dict_result) """
+
+#Practice 12 查找元组中元素的索引
+
+tuple_gy = ("Garry","Chili","Vincent","Joe Tian",3,"Ryan","Allen",0,32)
+
+print(type(tuple_gy))
+
+print("The index of Vincent is ",tuple_gy.index("Vincent")," !")
+
+print(type({}),type([]))
+print(type(set()),type(()))
