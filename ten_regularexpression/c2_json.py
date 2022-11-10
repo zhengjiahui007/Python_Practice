9d59ac476e7cb0321891035cce6fd4e0e1200d0e
ZJW <330431976@qq.com>
2022-11-10 22:53:49 +0800

    update the json chapter

---
+++ ten_regularexpression/c2_json.py
@@ -0,0 +1,38 @@
+import json
+# 反序列化 序列化
+""" 
+gy_json_str = '{"Name":"Garry","age":18}'
+gy_student = json.loads(gy_json_str)
+print(type(gy_student))
+print(gy_student)
+
+
+gy_json_str = '[{"Name":"Garry","age":18,"Flag":False},{"Name":"Lucy","age":28}]'
+gy_student = json.loads(gy_json_str)
+print(type(gy_student))
+print(gy_student)
+ """
+
+""" 
+student_list = [
+                {"Name":"Garry","age":18,"Flag":False},
+                {"Name":"Lucy","age":28}
+               ]
+
+gy_js_str = json.dumps(student_list)
+print(type(gy_js_str))
+print(gy_js_str)
+
+ """
+
+ # JSON 对象
+ # JSON
+ # JSON 字符串
+
+
+
+
+
+
+
+
