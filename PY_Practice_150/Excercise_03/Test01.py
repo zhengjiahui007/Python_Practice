82231a0e17dca8dd9444a8ae2f1ae1c48702e724
ZJW <330431976@qq.com>
2024-06-04 01:11:15 +0800

    upload the ex 03 Test01.py

---
+++ PY_Practice_150/Excercise_03/Test01.py
@@ -0,0 +1,132 @@
+# !/usr/bin/env python3
+# -*- coding : utf-8 -*-
+# !__author__ : "GarryZheng"
+# Date : 2024-06-01
+
+import os,sys,time,math,copy
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+from Test_com import py_test_exit
+
+def gy_abs(number):
+    if isinstance(number,(int,float)):
+        if (0 <= number):
+            return number
+        else:
+            return (-1*number)
+    else:
+        print("The number is not correct !")
+    return None
+
+def gy_sum(lst):
+    list_sum = 0
+    if isinstance(lst,list):
+        for i_val in lst:
+            if isinstance(i_val,(int,float)):
+                list_sum += i_val
+    return list_sum
+
+def gy_max(sequence):
+    if ((0 == len(sequence)) or (not isinstance(sequence,(list,tuple)))):
+        print("The sequence is empty or not a sequence !")
+        return None
+
+    list_digit = []
+    for i_val in sequence:
+        if isinstance(i_val,(int,float)):
+            list_digit.append(i_val)
+
+    if (0 == len(list_digit)):
+        print("The list_digit is empty !")
+        return None
+
+    max_val = list_digit[0]
+    for i_val in list_digit:
+        if (i_val > max_val):
+            max_val = i_val
+    return max_val
+
+def gy_min(sequence):
+    '''
+    The same as gy_max
+    '''
+    pass
+    return None
+
+def gy_int(str_val:str):
+    if not isinstance(str_val,(str,)):
+        print(f"The val {str_val} in not a string !")
+        return None
+
+    if not str_val.isdigit():
+        print(f"The val {str_val} in not a digital string !")
+        return None        
+    #如果步长为负数 , 其起始下标索引 要 大于 结束下标索引 ,当你使用步长-1时，默认的起始值是列表的最后一个元素
+    #print(str_val[ : -len(str_val) - 1  :-1])
+    int_dic = {
+        '0' : 0,
+        '1' : 1,
+        '2' : 2,
+        '3' : 3,
+        '4' : 4,
+        '5' : 5,
+        '6' : 6,
+        '7' : 7,
+        '8' : 8,
+        '9' : 9
+    }
+    int_val = 0
+    for i_val in str_val:
+        int_val *= 10
+        int_val += int_dic[i_val]
+    return int_val
+
+
+def py_test_3_1():
+    print(gy_abs(0.4))
+    print(gy_abs(23))
+    print(gy_abs(-56))
+    print(gy_abs(-0.4))
+    return None
+
+def py_test_3_2():
+    print(gy_sum([1,67,32,'2',23,"sw"]))
+    return None
+
+def py_test_3_3():
+    print(gy_max([1,67,32,'2',23,"sw"]))
+    print(gy_max([-1,167,32,'12',0,"sw"]))
+    print(gy_max(['-1',"167","32",'12',"0","sw"]))
+    print(gy_max("fda"))
+    return None
+
+def py_test_3_4():
+    num_result = gy_int("54893")
+    print(num_result,type(num_result))
+    num_result = gy_int("12231")
+    print(num_result,type(num_result))    
+    num_result = gy_int("5")
+    print(num_result,type(num_result))    
+    return None
+
+
+
+if ('__main__' == __name__):
+    dis_playmessage = """
+    0.Exit.
+    1.py_test_3_1.
+    2.py_test_3_2.
+    3.py_test_3_3.
+    4.py_test_3_4.
+    """
+    test_dict = {
+        "0" : py_test_exit,
+        "1" : py_test_3_1,
+        "2" : py_test_3_2,
+        "3" : py_test_3_3,
+        "4" : py_test_3_4
+    }
+    print(dis_playmessage)
+    while True:
+        gy_choice = input("Please input the test choice : ")
+        test_dict[gy_choice]()
