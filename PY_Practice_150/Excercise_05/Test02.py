cd2c0db604ec6d3f81ebd8f6030d0c4dd6aee834
ZJW <330431976@qq.com>
2024-07-25 23:48:47 +0800

    upload the test 6_15 to 6_22

--- PY_Practice_150/Excercise_05/Test02.py
+++ PY_Practice_150/Excercise_05/Test02.py
@@ -178,7 +178,102 @@ def gy_test_6_14():
         print(character_info_dic)
     except Exception as e:
         print(f"An error {e} occurred !")
+    return
 
+def gy_test_6_15():
+    gy_N = int(input("Please input an integer : "))
+    start_N = (10 ** (gy_N - 1))
+    end_N = (10 ** (gy_N))
+    gy_lst = []
+    for i_n in range(start_N,end_N,1):
+        sum_n = 0
+        temp_n = i_n
+        for div_count in range(0,gy_N,1):
+            sum_n += (temp_n % 10) ** gy_N
+            temp_n //= 10
+        if (sum_n == i_n):
+            gy_lst.append(i_n)
+    print(gy_lst)
+    return
+
+def gy_test_6_16():
+    gy_N = int(input("Please input an integer : "))
+    gy_lst = []
+    for i_n in range(1,gy_N + 1,1):
+        if (gy_N >= (i_n ** 2)):
+            gy_lst.append(i_n ** 2)
+        else:
+            break
+    print(gy_lst)
+    return
+
+def gy_test_6_17():
+    gy_lst = [3,5,1,2,1,3]
+    gy_result_set = set()
+    for i_Hundred in gy_lst:
+        for i_Ten in gy_lst:
+            for i_unit in gy_lst:
+                if ((i_unit not in (i_Hundred,i_Ten)) and (i_Hundred != i_Ten)):
+                    gy_result_set.add(i_Hundred * 100 + i_Ten * 10 + i_unit)
+    print(gy_result_set)
+    return
+
+def gy_test_6_18():
+    gy_lst = [23,342,12,65,2,3213,65,93,2]
+    print("Original lst = ",gy_lst)
+    gy_lst_revert = []
+    for i in range(0,len(gy_lst)//2,1):
+        gy_lst[len(gy_lst) - i - 1], gy_lst[i] = gy_lst[i],gy_lst[len(gy_lst) - i - 1]
+    print("Revert lst = ",gy_lst)
+    return
+
+def gy_test_6_19():
+    gy_N = int(input("Please input the offset number : "))
+    gy_lst = [23,342,12,65,2,3213,65,93,2]
+    print("Original lst = ",gy_lst)
+    # gy_lst_offsetright_2 = gy_lst[-1:-3:-1] + gy_lst[0:len(gy_lst) - 2:1]
+    offset_N = gy_N % len(gy_lst)
+    for i in range(0,offset_N,1):
+        temp_val = gy_lst[-1]
+        gy_lst.pop(-1)
+        gy_lst.insert(0,temp_val)
+    print(f"gy_lst offset {gy_N} {gy_lst} .")
+    return
+
+def gy_test_6_20():
+    gy_str = (input("Please input 3 number split with space like 1 2 3 : "))
+    gy_lst = gy_str.split(" ")
+    gy_lst_int = []
+    for i_char in gy_lst:
+        gy_lst_int.append(int(i_char))
+    for i in range(0,len(gy_lst_int),1):
+        for j in range(0,len(gy_lst_int) - 1 - i,1):
+            if (gy_lst_int[j] < gy_lst_int[j + 1]):
+                gy_lst_int[j],gy_lst_int[j + 1] = gy_lst_int[j + 1],gy_lst_int[j]
+    for i in gy_lst_int:
+        print(i,end = " ")
+    return
+
+def gy_test_6_21():
+    dict_gy_score = {
+        "python" : 590,
+        "c++" : 189,
+        "java" : 397
+    }
+    max_score,max_score_key = dict_gy_score["python"],"python"
+    for gy_key,gy_val in dict_gy_score.items():
+        if (gy_val > max_score):
+            max_score,max_score_key = gy_val,gy_key
+    print(f"The highest score is {max_score_key} : {max_score} .")
+    return
+
+def gy_test_6_22():
+    ming_fav_fruit = {"Banana","Apple","Grape"}
+    hong_fav_fruit = {"Banana","Strawberry","Peach"}
+    print(f"Ming and Hong like both {ming_fav_fruit & hong_fav_fruit}")
+    print(f"All of the fav fruits for Ming and Hong are {ming_fav_fruit | hong_fav_fruit}")
+    print(f"The fav fruits for Ming but not Hong are {ming_fav_fruit - (ming_fav_fruit & hong_fav_fruit)}")
+    print(f"The fav fruits for Hong but not Ming are {hong_fav_fruit - (ming_fav_fruit & hong_fav_fruit)}")
     return
 
 if (__name__ == "__main__"):
@@ -191,6 +286,14 @@ if (__name__ == "__main__"):
     5 : gy_test_6_12(Print rhombus)
     6 : gy_test_6_13(Print 9 * 9 table)
     7 : gy_test_6_14(Count the character)
+    8 : gy_test_6_15(Narcissistic number)
+    9 : gy_test_6_16(Perfect Square number)
+    10 : gy_test_6_17
+    11 : gy_test_6_18(Revert list)
+    12 : gy_test_6_19(List offset)
+    13 : gy_test_6_20(Sort list)
+    14 : gy_test_6_21(Get the highest score)
+    15 : gy_test_6_22
     '''
     dic_input = {
         "0" : py_test_exit,
@@ -200,7 +303,15 @@ if (__name__ == "__main__"):
         "4" : gy_test_6_11,
         "5" : gy_test_6_12,
         "6" : gy_test_6_13,
-        "7" : gy_test_6_14
+        "7" : gy_test_6_14,
+        "8" : gy_test_6_15,
+        "9" : gy_test_6_16,
+        "10" : gy_test_6_17,
+        "11" : gy_test_6_18,
+        "12" : gy_test_6_19,
+        "13" : gy_test_6_20,
+        "14" : gy_test_6_21,
+        "15" : gy_test_6_22
     }
     while True:
         print(dis_playmessage)
