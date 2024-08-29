4fb460a687de88dde71cd64265ca43b07d531cd4
ZJW <330431976@qq.com>
2024-08-29 22:56:12 +0800

    upload the gy_test_7_10

--- PY_Practice_150/Excercise_06/Test01.py
+++ PY_Practice_150/Excercise_06/Test01.py
@@ -281,28 +281,144 @@ def gy_test_7_8():
     print(gy_add_bin_str(gy_bin_str_0,gy_bin_str_1))
     return
 
+def gy_find_firstword(str_input:str):
+    try:
+        for i_val in str_input:
+            if (('a' > i_val) or ('z' < i_val)):
+                print(f"The string is not correct {str_input}!")
+                return (-1,"")
+
+        for i in range(0,len(str_input),1):
+            i_val = str_input[i]
+            new_str = str_input.replace(i_val," ",1)
+            if i_val not in new_str:
+                return (i,i_val)
+
+    except Exception as e:
+        print(f"An error occured {e}")
+    return (-1,"")
+
+def gy_test_7_9():
+    print("The first lettet which appearing only once is ",gy_find_firstword("ureoiwufjdasdlklfksj"))
+    print("The first lettet which appearing only once is ",gy_find_firstword("abcacd"))
+    print("The first lettet which appearing only once is ",gy_find_firstword("eettgfaq"))
+    return
+
+def gy_plus_two_strings(str_0:str,str_1:str)->str:
+    if ((not str_0.isdigit()) or (not str_1.isdigit())):
+        return None
+
+    gy_len_0 = len(str_0)
+    gy_len_1 = len(str_1)
+    new_str_0 = ""
+    new_str_1 = ""
+    diff_0 = 0
+    if (gy_len_0 >= gy_len_1):
+        diff_0 = gy_len_0 - gy_len_1
+        new_str_1 = '0' * diff_0 + str_1
+        new_str_0 = str_0
+    else:
+        diff_0 = gy_len_1 - gy_len_0
+        new_str_0 = '0' * diff_0 + str_0
+        new_str_1 = str_1
+    # print(f"new_str_0 = {new_str_0} , new_str_1 = {new_str_1} !")
+    gy_carry_num = 0
+    gy_sum_bit = 0
+    gy_sum_lst = []
+    for index in range((len(new_str_0) - 1),-1,-1):
+        gy_sum_bit = 0
+        gy_sum_bit = int(new_str_0[index]) + int(new_str_1[index]) + gy_carry_num
+        gy_carry_num = gy_sum_bit//10
+        gy_sum_bit = gy_sum_bit%10
+        gy_sum_lst.insert(0,str(gy_sum_bit))
+        if (0 == index):
+            if (gy_carry_num != 0):
+               gy_sum_lst.insert(0,str(gy_carry_num)) 
+    # print("gy_sum_lst = ",gy_sum_lst)
+    gy_sum_str = "".join(gy_sum_lst)
+    return gy_sum_str
+
+def gy_multi_strs(str_0:str,str_1:str)->str:
+    if ((not str_0.isdigit()) or (not str_1.isdigit())):
+        return None
+    '''
+    str_0 * str_1[i]:
+        str_0[j] * str_1[i],store the result as string,need * 10(n - 1) , then sum the list as one result str_0 * str_1[i]
+    sum the list of all the str_0 * str_1[i],get the total result
+    '''
+    str_0_reversed = str_0[::-1]
+    str_1_reversed = str_1[::-1]
+    # store str_0[j] * str_1[i] result list
+    multi_single_result = []
+    single_multi_single_result = 0
+    ten_multi_str0 = 1
+    ten_multi_str1 = 1
+    # print(str_0," * ",str_1)
+     # store str_0 * str_1[i] result list
+    multi_single_sum_result = []
+    for i_val in str_1_reversed:
+        multi_single_result = []
+        ten_multi_str0 = 1
+        for j_val in str_0_reversed:
+            single_multi_single_result = int(j_val) * int(i_val)
+            multi_single_result.append(str(single_multi_single_result) + (ten_multi_str0 - 1) * '0')
+            single_multi_single_result = 0
+            ten_multi_str0 += 1
+        # print("multi_single_result = ",multi_single_result)
+        plus_res = multi_single_result[0]
+        for i in range(1,len(multi_single_result),1):
+            plus_res = gy_plus_two_strings(plus_res,multi_single_result[i])
+        # print("plus_res = ",plus_res)
+        multi_single_sum_result.append(plus_res + (ten_multi_str1 - 1) * '0')
+        ten_multi_str1 += 1
+        # print("multi_single_sum_result = ",multi_single_sum_result)
+    total_result = multi_single_sum_result[0]
+    for j in range(1,len(multi_single_sum_result),1):
+        total_result = gy_plus_two_strings(total_result,multi_single_sum_result[j])
+    return total_result
+
+def gy_test_7_10():
+    a = "678"
+    b = "89"
+    print(f"The {a} * {b} = {gy_multi_strs(a,b)} ")
+    # print("The result is ",gy_multi_strs("6078","19089"))
+    a = "6078"
+    b = "19089"
+    print(f"The {a} * {b} = {gy_multi_strs(a,b)} ")
+    a = "123424234132423416078"
+    b = "4314376574569345657819089"
+    print("The {0} * {1} = {2} ".format(a,b,gy_multi_strs(a,b)))
+    a = "123424234132423416078"
+    b = "90"
+    print("The {0} * {1} = {2} ".format(a,b,gy_multi_strs(a,b)))
+    return
+
 if (__name__ == "__main__"):
     dis_playmessage = '''
-    0 : Exit
-    1 : gy_test_7_1
-    2 : gy_test_7_2
-    3 : gy_test_7_3
-    4 : gy_test_7_4
-    5 : gy_test_7_5
-    6 : gy_test_7_6
-    7 : gy_test_7_7
-    8 : gy_test_7_8
+    00 : Exit
+    01 : gy_test_7_1
+    02 : gy_test_7_2
+    03 : gy_test_7_3
+    04 : gy_test_7_4
+    05 : gy_test_7_5
+    06 : gy_test_7_6
+    07 : gy_test_7_7
+    08 : gy_test_7_8
+    09 : gy_test_7_9
+    10 : gy_test_7_10
     '''
     dic_input = {
-        "0" : py_test_exit,
-        "1" : gy_test_7_1,
-        "2" : gy_test_7_2,
-        "3" : gy_test_7_3,
-        "4" : gy_test_7_4,
-        "5" : gy_test_7_5,
-        "6" : gy_test_7_6,
-        "7" : gy_test_7_7,
-        "8" : gy_test_7_8
+        "0"  : py_test_exit,
+        "01" : gy_test_7_1,
+        "02" : gy_test_7_2,
+        "03" : gy_test_7_3,
+        "04" : gy_test_7_4,
+        "05" : gy_test_7_5,
+        "06" : gy_test_7_6,
+        "07" : gy_test_7_7,
+        "08" : gy_test_7_8,
+        "09" : gy_test_7_9,
+        "10" : gy_test_7_10
     }
     while True:
         print(dis_playmessage)
