ea3e0b7e410982cd42ebb1a7105ede7e6f37629d
ZJW <330431976@qq.com>
2025-03-04 22:47:21 +0800

    upload using class to draw a ball

--- LinuxPythonWorkshop/Python_EveryDay/Py_20241129.py
+++ LinuxPythonWorkshop/Python_EveryDay/Py_20241129.py
@@ -42,6 +42,41 @@ def gy_main():
 use the Pygame
 '''
 import pygame,random
+from enum import Enum,unique
+from math import sqrt
+from random import randint
+
+@unique
+class Gy_color(Enum):
+    gy_Red = (255,0,0)
+    gy_Green = (0,255,0)
+    gy_Blue = (0,0,255)
+    gy_Black = (0,0,0)
+    gy_White = (255,255,255)
+    gy_Grey = (242,242,242)
+
+    @staticmethod
+    def gy_random_color() -> tuple:
+        gy_red = randint(0,255)
+        gy_green = randint(0,255)
+        gy_blue = randint(0,255)
+        return (gy_red,gy_green,gy_blue)
+
+class GY_Ball(object):
+    def __init__(self,x,y,radius,sx,sy,color = Gy_color.gy_Red) -> None:
+        self.gy_x = x
+        self.gy_y = y
+        self.gy_r = radius
+        self.gy_sx = sx
+        self.gy_sy = sy
+        self.gy_color = color
+        self.gy_alive = True
+        return
+
+    def gy_move(self,screen) -> None:
+        self.gy_x += self.gy_sx
+        self.gy_y += self.gy_sy
+        return
 
 def gy_py_main():
     print(pygame.__version__)
@@ -84,10 +119,30 @@ def gy_pymove_main():
 
     return
 
+def gy_ballmove_main():
+    pygame.init()
+    gy_screen = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("Big ball eats small ball!")
+    gy_ball = GY_Ball(50,50,20,10,5,Gy_color.gy_random_color())
+    gy_running = True
+    while gy_running:
+        for gy_event in pygame.event.get():
+            #print("The event type is = {}".format(gy_event.type))
+            if (pygame.QUIT == gy_event.type):
+                gy_running = False
+        gy_screen.fill((242,242,242))
+        pygame.draw.circle(gy_screen,gy_ball.gy_color,(gy_ball.gy_x,gy_ball.gy_y),gy_ball.gy_r,0)
+        pygame.display.flip()
+        pygame.time.delay(500)
+        gy_ball.gy_move(gy_screen)
+
+    return
+
 if "__main__" == __name__:
     #gy_main()
     #gy_py_main()
-    gy_pymove_main()
+    #gy_pymove_main()
+    gy_ballmove_main()
 
 '''
 def gy_main():
