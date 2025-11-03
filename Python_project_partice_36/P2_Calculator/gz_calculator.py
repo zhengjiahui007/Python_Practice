049cc9c69bd62dcf127492771a46a05d037c3b03
ZJW <330431976@qq.com>
2025-11-03 23:05:59 +0800

    Add the debug print fuction ,add the operator function

--- Python_project_partice_36/P2_Calculator/gz_calculator.py
+++ Python_project_partice_36/P2_Calculator/gz_calculator.py
@@ -6,6 +6,19 @@
 
 import tkinter,math,os,sys
 from functools import partial
+import inspect
+
+def gz_debug_print(message):
+    """打印当前上下文信息"""
+    caller_frame = inspect.currentframe().f_back
+    file_name = caller_frame.f_code.co_filename
+    line_num = caller_frame.f_lineno
+    func_name = caller_frame.f_code.co_name
+    
+    if message:
+        print(f"[DEBUG] {func_name} - {line_num} : {message}")
+    else:
+        print(f"[DEBUG] : {func_name} - {line_num} ")
 
 class GZ_Calculator:
     def __init__(self,title:str,x:int,y:int,w:int,h:int) -> None:
@@ -51,12 +64,77 @@ class GZ_Calculator:
         self.gz_CurrentShow.set('0')
         return None
 
+    '''按下小数点'''
+    def gz_cal_pressDP(self) -> None:
+        try:
+            if self.is_calc:
+                self.gz_CurrentShow.set('0')
+                self.is_calc = False
+            if self.gz_CurrentShow.get().find('.') == -1:
+                if len(self.gz_CurrentShow.get()) < self.max_show_len:
+                    self.gz_CurrentShow.set(self.gz_CurrentShow.get() + '.')
+        except Exception as e:
+            print('An exception occurred ',e)
+        return None
+
+    '''计算答案修正'''
+    def gz_cal_modifyResult(self,result) -> str:
+        result = str(result)
+        if len(result) > self.max_show_len:
+            if len(result.split('.')[0]) > self.max_show_len:
+                result = 'Overflow'
+            else:
+                # 直接舍去不考虑四舍五入问题
+                result = result[0 : self.max_show_len : ]
+        return result
+
     def gz_cal_show(self) -> None:
         self.root_sc.mainloop()
         return None
 
     def gz_cal_pressOperator(self,oper:str) -> None:
-        print("gz_cal_pressOperator : ",oper)
+        gz_debug_print("The operator is {}".format(oper))
+        if (oper == '+/-'):
+            try:
+                if self.gz_CurrentShow.get().startswith('-'):#用于检查字符串是否是以指定子字符串开头，如果是则返回 True，否则返回 False
+                    self.gz_CurrentShow.set(self.gz_CurrentShow.get()[1::])
+                else:
+                    self.gz_CurrentShow.set('-' + self.gz_CurrentShow.get())
+            except Exception as e:
+              gz_debug_print('An exception occurred {} '.format(e))
+
+        elif (oper == '1/x'):
+            try:
+                result = 1 / float(self.gz_CurrentShow.get())
+            except Exception as e:
+                gz_debug_print('An exception occurred {} '.format(e))
+                result = 'illegal operation'
+            result = self.gz_cal_modifyResult(result)
+            self.gz_CurrentShow.set(result)
+            self.is_calc = True
+        elif oper == 'sqrt':
+            try:
+                result = math.sqrt(float(self.gz_CurrentShow.get()))
+            except Exception as e:
+                gz_debug_print('An exception occurred {} '.format(e))
+                result = 'illegal operation'
+            result = self.gz_cal_modifyResult(result)
+            self.gz_CurrentShow.set(result)
+            self.is_calc = True
+        elif (oper == 'MC'):
+            self.gz_storage.clear()
+        elif (oper == 'MR'):
+            if self.is_calc:
+                self.gz_CurrentShow.set('0')
+            self.gz_storage.append(self.gz_CurrentShow.get())
+            expression = ''.join(self.gz_storage)
+            try:
+                result = eval(expression)
+            except:
+                result = 'illegal operation'
+            result = self.gz_cal_modifyResult(result)
+            self.gz_CurrentShow.set(result)
+            self.is_calc = True
         return None
 
     def gz_cal_pressNumber(self,number:str) -> None:
@@ -131,7 +209,7 @@ class GZ_Calculator:
             button_x = button_x + button_w_interval
 
         # line 4
-        button4_txt_lst = ["4","5","6","X","1/x"]
+        button4_txt_lst = ["4","5","6","*","1/x"]
         button_x = 20
         button4_1_y = button3_1_y + button_h_interval
         for b_txt_i in range(0,len(button4_txt_lst),1):
@@ -178,7 +256,10 @@ class GZ_Calculator:
             else:
                 button_bg = '#708069'
                 button_w = 87
-                command_button = partial(self.gz_cal_pressOperator,b_txt)
+                if ("." == b_txt):
+                    command_button = partial(self.gz_cal_pressDP)
+                else:
+                    command_button = partial(self.gz_cal_pressOperator,b_txt)
             button1 = tkinter.Button(self.root_sc,text = b_txt, bg = button_bg, bd = button_bd,command = command_button)
             button1.place(x = button_x, y = button6_1_y, width = button_w, height = button_h)
             if(0 >= b_txt_i):
